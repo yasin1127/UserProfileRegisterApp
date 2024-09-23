200ee289aef2f99980ec806a52d13eae37009783
pikoo <yasinarafat1127@gmail.com>
2024-09-23 13:32:21 +0600

    update

---
+++ app/src/main/java/com/example/uprapp/updateprofile.kt
@@ -0,0 +1,15 @@
+package com.example.uprapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class updateprofile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_updateprofile)
+    }
+}
